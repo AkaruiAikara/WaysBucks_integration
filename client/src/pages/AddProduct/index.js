5f3391446bfcd5fb4e297c9648654c3a03880a36
--get <vall@aikara.me>
2022-02-20 15:26:08 +0700

    products: Consume data from API

--- client/src/pages/AddProduct/index.js
+++ client/src/pages/AddProduct/index.js
@@ -1,50 +1,156 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { API } from "../../config/api";
+import Alert from "../../components/Alert";
+
 import product from "../../assets/img/product-big.png";
 import attachment from "../../assets/img/attachment.svg";
 
 export default function AddProduct() {
+  const params = useParams();
+  const [alert, setAlert] = useState(null);
+  // store data with useState as form
+  const [form, setForm] = useState({
+    title: null,
+    price: null,
+    image: null,
+  });
+  // get product data if on update page
+  useEffect(() => {
+    if (params.id) {
+      API.get(`/products/${params.id}`).then((res) => {
+        setForm({ ...res.data.data, image: null });
+        setPreview(res.data.data.image);
+      });
+    }
+  }, []);
+  const { title, price, image } = form;
+  const [preview, setPreview] = useState(image);
+  // handle input change
+  const handleChange = (e) => {
+    if (e.target.name === "image") {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPreview(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  // handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Set Content-Type to multipart/form-data into headers as config
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    // store data with FormData
+    const data = new FormData();
+    data.set("title", title);
+    data.set("price", price);
+    if (image && !(image === preview)) {
+      data.set(
+        "image",
+        document.getElementById("image").files[0],
+        document.getElementById("image").files[0].name
+      );
+    }
+    // update or insert product data to API
+    if (params.id) {
+      API.patch(`/products/${params.id}`, data, config)
+        .then((res) => {
+          setAlert({
+            status: "success",
+            message: "Product has been updated",
+          });
+        })
+        .catch((err) => {
+          if (err.response) {
+            setAlert(err.response.data);
+          }
+        });
+    } else {
+      API.post("/products", data, config)
+        .then((res) => {
+          setAlert({
+            status: "success",
+            message: "Product has been added",
+          });
+          setForm({
+            title: null,
+            price: null,
+            image: null,
+          });
+        })
+        .catch((err) => {
+          if (err.response) {
+            setAlert(err.response.data);
+          }
+        });
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col lg:flex-row gap-36">
         <div className="flex-grow">
-          <h2 className="text-3xl text-blood font-bold">Add Product</h2>
-          <div className="space-y-8 mt-12">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="bg-smooth border-2 border-blood text-gray-900 text-sm rounded-md focus:ring-red-800 focus:border-red-800 block w-full p-4 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="Name Product"
-              required
-            />
-            <input
-              type="number"
-              name="price"
-              id="price"
-              className="bg-smooth border-2 border-blood text-gray-900 text-sm rounded-md focus:ring-red-800 focus:border-red-800 block w-full p-4 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="Price"
-              required
-            />
-            <div className="relative">
-              <div className="flex absolute inset-y-0 right-0 items-center pr-6 pointer-events-none">
-                <img src={attachment} alt="" />
-              </div>
+          <form onSubmit={handleSubmit}>
+            <h2 className="text-3xl text-blood font-bold">
+              {params.id ? "Update" : "Add"} Product
+            </h2>
+            <Alert alert={alert} setAlert={setAlert} />
+            <div className="space-y-8 mt-12">
               <input
-                type="file"
-                name="name"
-                id="name"
-                className="bg-smooth border-2 border-blood text-gray-900 text-sm rounded-md focus:ring-red-800 focus:border-red-800 block w-full p-4 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white file:hidden"
-                placeholder="Name Product"
+                type="text"
+                name="title"
+                id="title"
+                value={title}
+                onChange={handleChange}
+                className="bg-smooth border-2 border-blood text-gray-900 text-sm rounded-md focus:ring-red-800 focus:border-red-800 block w-full p-4 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="Title Product"
                 required
               />
+              <input
+                type="number"
+                name="price"
+                id="price"
+                value={price}
+                onChange={handleChange}
+                className="bg-smooth border-2 border-blood text-gray-900 text-sm rounded-md focus:ring-red-800 focus:border-red-800 block w-full p-4 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="Price"
+                required
+              />
+              <div className="relative">
+                <div className="flex absolute inset-y-0 right-0 items-center pr-6 pointer-events-none">
+                  <img src={attachment} alt="" />
+                </div>
+                <input
+                  type="file"
+                  name="image"
+                  id="image"
+                  onChange={handleChange}
+                  className="cursor-pointer bg-smooth border-2 border-blood text-gray-900 text-sm rounded-md focus:ring-red-800 focus:border-red-800 block w-full p-4 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white file:hidden"
+                />
+              </div>
             </div>
-          </div>
-          <div className="flex justify-center">
-            <button className="text-center text-white bg-blood w-[90%] py-2.5 mt-20 rounded-md hover:bg-red-600 focus:bg-red-900 focus:ring-4 focus:ring-red-200">
-              Add Product
-            </button>
-          </div>
+            <div className="flex justify-center">
+              <button className="text-center text-white bg-blood w-[90%] py-2.5 mt-20 rounded-md hover:bg-red-600 focus:bg-red-900 focus:ring-4 focus:ring-red-200">
+                {params.id ? "Update" : "Add"} Product
+              </button>
+            </div>
+          </form>
         </div>
-        <img src={product} alt="" />
+        <img
+          className="w-[436px] h-[555px] object-cover rounded-lg"
+          src={preview ?? product}
+          alt=""
+        />
       </div>
     </>
   );
--- client/src/pages/Dashboard/Product/index.js
+++ client/src/pages/Dashboard/Product/index.js
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { API } from "../../../config/api";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 export default function Product() {
+  const [products, setProducts] = useState([]);
+  // get all products
+  useEffect(() => {
+    API.get("/products").then((res) => {
+      setProducts(res.data.data.products);
+    });
+  }, []);
+  const MySwal = withReactContent(Swal);
+  const deleteProduct = (id) => {
+    MySwal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+    }).then((result) => {
+      if (result.value) {
+        API.delete(`/products/${id}`).then((res) => {
+          MySwal.fire("Deleted!", "Your file has been deleted.", "success");
+          setProducts(products.filter((product) => product.id !== id));
+        });
+      }
+    });
+  };
+  // function that separate every 3 digits with dot
+  const dot = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
   return (
     <>
       <div className="flex justify-between">
@@ -51,7 +83,47 @@ export default function Product() {
                     </th>
                   </tr>
                 </thead>
-                <tbody></tbody>
+                <tbody>
+                  {products.map((product, index) => (
+                    <tr
+                      key={product.id}
+                      className="bg-white border dark:bg-gray-800"
+                    >
+                      <td className="py-4 px-6 border text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                        {index + 1}
+                      </td>
+                      <td className="py-4 px-6 border text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                        {product.title}
+                      </td>
+                      <td className="py-4 px-6 border text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                        {dot(product.price)}
+                      </td>
+                      <td className="py-4 px-6 border text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                        <img
+                          className="w-[43px] h-[55px] object-cover rounded-lg hover:absolute hover:w-[215px] hover:h-[275px] top-1/2 bottom-auto"
+                          src={product.image}
+                          alt="image"
+                        />
+                      </td>
+                      <td className="py-4 px-6 border text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                        <div className="flex gap-8 justify-around">
+                          <Link
+                            to={"" + product.id}
+                            className="flex-1 py-1 bg-sky-500 text-white text-center hover:bg-sky-800 rounded-xl"
+                          >
+                            <i class="bi bi-pencil-square"></i>
+                          </Link>
+                          <button
+                            onClick={() => deleteProduct(product.id)}
+                            className="flex-1 py-1 bg-red-500 text-white hover:bg-red-800 rounded-xl"
+                          >
+                            <i class="bi bi-trash3"></i>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
               </table>
             </div>
           </div>
